63da444304707303eee56b5b9f580176468e8ac7
Valtteri Asikainen <valtteri.asikainen@paivola.fi>
2019-04-02 01:18:37 +0300

    Small changes to event_handler.py #2

--- pythonlike/src/event_handler.py
+++ pythonlike/src/event_handler.py
@@ -6,18 +6,29 @@ def handle_events():
         if x.type == "QUIT":
             return "quit"
         elif x.type == "KEYDOWN":
-            key = x.scancode
-            return handle_keydown(key)
+            return handle_keydown(x.scancode, x.mod)
         else:
             pass
     return None
 
 
-def handle_keydown(key):
+def handle_keydown(key, mod):
     if key in range(89, 98):
-        direction = {89: "SW", 90: "S", 91: "SE", 92: "W", 93: "STAY", 94: "W", 95: "NW", 96:  "N", 97: "NE"}
-        return "move {}".format(direction[key])
+        direction = {89: "SW", 90: "S", 91: "SE", 92: "W", 93: "STAY", 94: "E", 95: "NW", 96:  "N", 97: "NE"}
+        return "move {}".format(direction[key])  # NUM_5 only works with numlock on (returns different code)
     else:
-        return key
+        return key, mod
 
 
+'''
+LSHIFT		1
+RSHIFT		2
+LCTRL		64
+RCTRL		128
+LALT		256
+RALT		512
+LGUI		1024 left win/meta
+RGUI		2048 right win/meta
+NUMLOCK		4096
+CAPSLOCK	8192
+'''
