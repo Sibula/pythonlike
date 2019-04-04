a400c3dfdb1487fd88a42b0d78bf019b726d39fa
Valtteri Asikainen <valtteri.asikainen@paivola.fi>
2019-04-04 13:47:50 +0300

    Starting work on a combat handler #22

---
+++ pythonlike/src/combat.py
@@ -0,0 +1,14 @@
+def attack(attacker, defender, method, entities):
+    # Choose handler by method
+
+    # Calculate attack values
+
+    # Calculate defence values
+
+    # Calculate result (did it hit -> how much damage -> did it kill)
+
+    # Construct message
+
+    pass
+
+# Should we have a class for combat results?
