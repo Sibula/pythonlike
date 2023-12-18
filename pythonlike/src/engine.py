bed836290b152ca62f78c652cfc95537903704f5
Valtteri Asikainen <valtteri.asikainen@tuni.fi>
2023-12-18 17:31:58 +0200

    Move map related functions from Engine to GameMap

--- pythonlike/src/engine.py
+++ pythonlike/src/engine.py
@@ -44,18 +44,17 @@ class Engine:
     def _invalid(self, param):
         return None  # "Invalid command: {}".format(param)
 
-
     def _move(self, action: Action) -> str | None:
-        player = self.get_player()
-        index = self.get_entity_index(player.x, player.y)
+        player = self.game_map.player
+        index = self.game_map.get_entity_index(player.x, player.y)
         dx, dy = action.param
         nx, ny = player.x + dx, player.y + dy
-        occupied = self.is_occupied(nx, ny)
+        occupied = self.game_map.is_occupied(nx, ny)
         nt = self.game_map.tiles[nx, ny]
         if nt["walkable"] and not occupied:
             player.x, player.y = nx, ny
         elif occupied:
-            return attack(index, self.get_entity_index(nx, ny), self.game_map.entities)
+            return attack(index, self.game_map.get_entity_index(nx, ny), self.game_map.entities)
         elif nt == tile.door:
             # game_map.tiles[nx, ny].interact()
             pass
@@ -63,42 +62,8 @@ class Engine:
     def _stay(self) -> None:
         return None
 
-
     def _interact(self) -> str:
         return "interact"
 
-
     def _loot(self) -> str:
         return "loot"
-
-
-    def is_occupied(self, x: int, y: int) -> bool:
-        # Check if the tile at (x, y) is occupied by an entity.
-        has_entity = False
-        for ent_x, ent_y in [(ent.x, ent.y) for ent in self.game_map.entities]:
-            if (ent_x, ent_y) == (x, y):
-                has_entity = True
-
-        return has_entity
-
-
-    def get_entity(self, x: int, y: int) -> entity.Entity | None:
-        # Return entity at (x, y).
-        for ent in self.game_map.entities:
-            if (ent.x, ent.y) == (x, y):
-                return ent
-
-
-    def get_entity_index(self, x: int, y: int) -> int | None:
-        # Return index of entity at (x, y) in entities.
-        for i, ent in enumerate(self.game_map.entities):
-            if (ent.x, ent.y) == (x, y):
-                return i
-        return None
-
-
-    def get_player(self) -> entity.Player:
-        # Find the Player object from entities and return it.
-        for ent in self.game_map.entities:
-            if type(ent) == entity.Player:
-                return ent
--- pythonlike/src/game_map.py
+++ pythonlike/src/game_map.py
@@ -8,8 +8,28 @@ class GameMap:
         self.h = h
         self.tiles = tiles
         self.entities = entities
+        self.player = entities[0]  # Player should always be first when initializing creatures
+        # self.objects = []
+        # self.items = []
         self.explored = np.zeros_like(self.tiles)
         self.visible = np.zeros_like(self.tiles)
 
     def is_walkable(self, x: int, y: int) -> bool:
         return self.tiles[x, y]["walkable"]
+    
+    def is_occupied(self, x: int, y:int) -> bool:
+        return True if self.get_creature(x, y) else False
+
+    def get_creature(self, x: int, y: int) -> Entity | None:
+        '''Return creature at (x,y) or None if there is no creature.'''
+        for e in self.entities:
+            if e.x == x and e.y == y:
+                return e
+        return None
+    
+    def get_creature_index(self, x: int, y: int) -> int | None:
+        '''Return index of entity at (x, y) in entities.'''
+        for i, e in self.entities:
+            if e.x == x and e.y == y:
+                return i
+        return None
