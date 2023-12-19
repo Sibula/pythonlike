c024037c6d2127b137cf67774223359cb2a8e60f
Valtteri Asikainen <valtteri.asikainen@tuni.fi>
2023-12-19 17:41:21 +0200

    Fix docstings, remove commented code

--- pythonlike/src/game_map.py
+++ pythonlike/src/game_map.py
@@ -21,14 +21,14 @@ class GameMap:
         return True if self.get_creature(x, y) else False
 
     def get_creature(self, x: int, y: int) -> Entity | None:
-        '''Return creature at (x,y) or None if there is no creature.'''
+        """Return creature at (x,y) or None if there is no creature."""
         for e in self.entities:
             if e.x == x and e.y == y:
                 return e
         return None
     
     def get_creature_index(self, x: int, y: int) -> int | None:
-        '''Return index of entity at (x, y) in entities.'''
+        """Return index of entity at (x, y) in entities."""
         for i, e in enumerate(self.entities):
             if e.x == x and e.y == y:
                 return i
